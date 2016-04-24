ee702748f35374c4269696ff42c5fd53114a6a4a
bmohabir <bmohabir01@gmail.com>
2016-04-24 17:36:37 -0400

    use properly sized background pizza image and move width and height from loop to style.css

--- views/js/main.js
+++ views/js/main.js
@@ -540,9 +540,8 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza3.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    elem.src = "images/pizza3.png"; // use properly sized image
+    // moved unnecessary styles from loop to .mover class in styles.css
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
